afb093449a90dccc26333ffe3ffc3b66ceeb3277
Utkarsh Jain <ujain@buyerfolio.ai>
2022-08-06 21:59:05 +0530

    Created Repositories

--- java-api/src/main/java/com/db/grad/javaapi/repository/BookUserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BookUserRepository.java
@@ -2,7 +2,10 @@ package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.BookUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface BookUserRepository extends JpaRepository<BookUser,Long> {
 
 }
--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
@@ -2,7 +2,10 @@ package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.Security;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface SecurityRepository extends JpaRepository<Security,Long> {
 
 }
