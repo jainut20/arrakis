770cf87a45b4abe8aef429f70bb2985420700827
Ketaki <urankarketaki@gmail.com>
2022-08-08 13:52:20 +0530

    sigin

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { Pets } from "./components/pets/Pets";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import SignUp from "./components/Signup";
 
 const App = () => {
-  return <Pets />;
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<SignUp/>} />
+      <Route path="/sign-in" element={<Login/>} />
+    </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ react-app/src/components/Signup.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import "../index.css"
+export default class SignUp extends Component {
+  render() {
+    return (
+      <Container className="col-md-5 mx-auto my-auto ">
+        <Card >
+          <form>
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+              <label>First name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Last name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Last name"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="mb-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="btn btn-primary">
+                Sign Up
+              </button>
+            </div>
+            <p className="forgot-password text-right">
+              Already registered <a href="/sign-in">sign in?</a>
+            </p>
+          </form>
+        </Card>
+      </Container>
+    );
+  }
+}
