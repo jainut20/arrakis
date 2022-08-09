ff82e920211731e216ae4c43cf7ff9a39cbd84e3
Tejas Pradhan <pradhan.tejas135@gmail.com>
2022-08-09 19:25:03 +0530

    added user debug service

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -4,12 +4,17 @@ INSERT INTO BOOK (id,bookname) VALUES (1,'TEJAS_BOOK');
 INSERT INTO BOOK (id,bookname) VALUES (2,'UTKARSH_BOOK');
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (1,'DY2TYT12','23EDJHH','RIL','2022-01-02','ABC','GOVT',1234.2,'ACTIVE');
+                     (1,'ABCE342','23EDJHH','RIL','2022-01-02','ABC','GOVT',1000,'ACTIVE');
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (2,'DY2TYT12','23EDJHH','DB','2020-12-01','ABC','GOVT',1234.2,'ACTIVE');
+                     (2,'DY2TYT12','23EDJHH','DB','2020-12-01','ABC','PVT',2000,'INACTIVE');
 
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (3,'DY2TYT12','23EDJHH','CS','2018-08-01','ABC','GOVT',1234.2,'ACTIVE');
+                     (3,'DY2TYT12','23EDJHH','CREDIT','2023-08-01','ABC','PVT',3500.2,'ACTIVE');
 
+INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
+                     (4,'DFHJH342','23EDJHH','RIL','2024-02-03','ABC','GOVT',5421.2,'ACTIVE');
+
+INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
+                     (5,'FURHF2351','23EDJHH','RIL','2022-01-02','ABC','PVT',4435.2,'ACTIVE');
