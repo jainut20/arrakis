f0b5452b52d59502d50d3d45b25884c50846eb53
Tejas Pradhan <pradhan.tejas135@gmail.com>
2022-08-10 13:18:55 +0530

    completed

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -5,20 +5,20 @@ INSERT INTO BOOK (id,bookname) VALUES (2,'UTKARSH_BOOK');
 
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (1,'ABCE342','23EDJHH','RIL','2022-01-02','ABC','GOVT',1000,'ACTIVE');
+                     (1,'ABCE342','23EDJHH','RIL','2022-01-02','6.5%','GOVT',1000,'ACTIVE');
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (2,'DY2TYT12','23EDJHH','DB','2020-12-01','ABC','PVT',2000,'INACTIVE');
+                     (2,'DY2TYT12','23EDJHH','DB','2020-12-01','3%','PVT',2000,'INACTIVE');
 
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (3,'DY2TYT12','23EDJHH','CREDIT','2023-08-01','ABC','PVT',3500.2,'ACTIVE');
+                     (3,'DY2TYT12','23EDJHH','CREDIT','2023-08-01','7.5%','PVT',3500.2,'ACTIVE');
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (4,'DFHJH342','23EDJHH','RIL','2024-02-03','ABC','GOVT',5421.2,'ACTIVE');
+                     (4,'DFHJH342','23EDJHH','RIL','2024-02-03','5%','GOVT',5421.2,'ACTIVE');
 
 INSERT INTO SECURITY (id,isin,cusip,issuer,maturitydate,coupon,type,facevalue,status) VALUES
-                     (5,'FURHF2351','23EDJHH','RIL','2022-01-02','ABC','PVT',4435.2,'ACTIVE');
+                     (5,'FURHF2351','23EDJHH','RIL','2022-01-02','8%','PVT',4435.2,'ACTIVE');
 
 INSERT INTO TRADE (id,bookid,counterpartyid,securityid, quantity, status, price, buysell, tradedate, settlementdate)
             VALUES (2,1,2,3,10,'COMPLETED',40.5,'SELL','2022-07-03','2022-07-04');
